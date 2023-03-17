5b53ff789ebdd1df6d9d0993bacc85eed9a86bf5
Yuuki1602 <kakashi16023@gmail.com>
2023-03-17 21:05:42 +0700

    router/layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/Layout";
+import { Fragment } from "react";
+
 function App() {
-
-
   return (
-    <div >
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout === null ? Fragment : DefaultLayout
+            const Page = route.component
+            return <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            />
+          })}
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
-export default App
+export default App;
