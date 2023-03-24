67e9e0d2fc87633254eb3a337d95007405635ece
Yuuki1602 <kakashi16023@gmail.com>
2023-03-24 12:24:01 +0700

    fix btn

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -5,11 +5,27 @@ import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
 import images from '~/assets/images'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical,faEarthAsia,faCircleQuestion,faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
 
 
 const cx = classNames.bind(styles)
@@ -58,9 +74,14 @@ function Header() {
                     <Button text>
                         Upload
                     </Button>
-                    <Button  primary >
+                    <Button primary >
                         Log in
                     </Button>
+                    <Menu items = {MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
