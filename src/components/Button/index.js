0571f13214877f5042c7a69559cf7a603da9ece5
Yuuki1602 <kakashi16023@gmail.com>
2023-03-24 12:15:19 +0700

    fix menu btn

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,33 +4,34 @@ import styles from './Button.module.scss'
 
 const cx = classNames.bind(styles)
 
-function Button({to, href, primary= false, outline=false, small = false, large=false, text=false,disabled = false,leftIcon,rightIcon , children, rounded =false, onClick, ...passProp}) {
-    let Comp ='button'
+function Button({ to, href, primary = false, outline = false, small = false, large = false, text = false, disabled = false, leftIcon, rightIcon, className, children, rounded = false, onClick, ...passProp }) {
+    let Comp = 'button'
     const props = {
         onClick,
         ...passProp,
     }
 
     //remove
-    if(disabled){
+    if (disabled) {
         Object.keys(props).forEach((key) => {
-            if(key.startsWith('on') && typeof props[key] === 'function'){
+            if (key.startsWith('on') && typeof props[key] === 'function') {
                 delete props[key]
             }
         })
     }
 
-    
 
-    if(to) {
+
+    if (to) {
         props.to = to
         Comp = Link
     }
-    else if(href){
+    else if (href) {
         props.href = href
         Comp = 'a'
     }
     const classes = cx('wrapper', {
+        [className]: className,
         primary,
         outline,
         small,
@@ -38,16 +39,14 @@ function Button({to, href, primary= false, outline=false, small = false, large=f
         text,
         disabled,
         rounded,
-        leftIcon,
-        rightIcon,
     })
-    return ( 
+    return (
         <Comp className={classes} {...props}>
             {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
             <span className={cx('title')}>{children}</span>
             {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
         </Comp>
-     );
+    );
 }
 
 export default Button;
